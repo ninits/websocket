5753d5afc68f25ed749908c42f7c573eeb06f94f
ni <riuf4718@gmail.com>
2020-03-31 22:30:19 +0800

    websocket: finished multiplayers and server send msg automatically

---
+++ README.txt
@@ -0,0 +1,4 @@
+建立websocket伺服器
+練習:
+1. 多人連線: 打開backoffice.html後，從client.html發訊息自動出現在backoffice.html(console中)
+2. 伺服器自動發送:利用setInterval定時發送 (在程式裡註解掉了)
---
+++ server.js
@@ -0,0 +1,40 @@
+//import express 和 ws 套件
+const express = require('express')
+const SocketServer = require('ws').Server
+
+//指定開啟的 port
+const PORT = 3000
+
+//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+//將 express 交給 SocketServer 開啟 WebSocket 的服務
+const wss = new SocketServer({ server })
+
+//當 WebSocket 從外部連結時執行
+wss.on('connection', ws => {
+
+    //連結時執行此 console 提示
+    console.log('Client connected')
+
+    // const sendNowTime = setInterval(()=>{
+    //     ws.send(String(new Date()))
+    // },1000)
+  
+    ws.on('message', data => {
+        //取得所有連接中的 client
+        let clients = wss.clients
+
+        //做迴圈，發送訊息至每個 client
+        clients.forEach(client => {
+            client.send(data)
+        })
+    })
+
+    //當 WebSocket 的連線關閉時執行
+    ws.on('close', () => {
+        // clearInterval(sendNowTime)
+        console.log('Close connected')
+    })
+})
